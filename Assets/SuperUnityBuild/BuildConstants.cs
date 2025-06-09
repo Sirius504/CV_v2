a98e19ff43736c0f74b6add2f23268914a9ce1f4
Igor <igor00901@yandex.ru>
2025-06-09 20:02:37 +0300

    Added SuperUnityBuild

---
+++ Assets/SuperUnityBuild/BuildConstants.cs
@@ -0,0 +1,48 @@
+using System;
+
+// This file is auto-generated. Do not modify or move this file.
+
+namespace SuperUnityBuild.Generated
+{
+    public enum ReleaseType
+    {
+        None,
+        NewReleaseType,
+    }
+
+    public enum Platform
+    {
+        None,
+        WebGL,
+    }
+
+    public enum ScriptingBackend
+    {
+        None,
+        IL2CPP,
+    }
+
+    public enum Target
+    {
+        None,
+        Player,
+    }
+
+    public enum Distribution
+    {
+        None,
+    }
+
+    public static class BuildConstants
+    {
+        public static readonly DateTime buildDate = new DateTime(638838052454066139);
+        public const string version = "1.0.0.1";
+        public const int buildCounter = 1;
+        public const ReleaseType releaseType = ReleaseType.NewReleaseType;
+        public const Platform platform = Platform.WebGL;
+        public const ScriptingBackend scriptingBackend = ScriptingBackend.IL2CPP;
+        public const Target target = Target.Player;
+        public const Distribution distribution = Distribution.None;
+    }
+}
+
