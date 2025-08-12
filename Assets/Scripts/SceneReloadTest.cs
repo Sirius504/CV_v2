6a448fbd2a2f67906fab66559b8552061da44502
Igor <igor00901@yandex.ru>
2025-08-12 13:34:32 +0300

    intermediate loading scene

--- Assets/Scripts/SceneReloadTest.cs
+++ Assets/Scripts/SceneReloadTest.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -5,14 +7,29 @@ using UnityEngine.UI;
 public class SceneReloadTest : MonoBehaviour
 {
     [SerializeField] private Button button;
+    private AsyncOperation operation;
 
     private void Start()
     {
+        DontDestroyOnLoad(transform.parent);
         button.onClick.AddListener(OnButtonClicked);
     }
 
     private void OnButtonClicked()
     {
-        SceneManager.LoadSceneAsync(0, LoadSceneMode.Single);
+        operation = SceneManager.LoadSceneAsync(1, LoadSceneMode.Single);
+        operation.completed += LoadNextScene;
+        //SceneManager.LoadSceneAsync(0, LoadSceneMode.Single);
+    }
+
+    private void LoadNextScene(AsyncOperation operation)
+    {
+        StartCoroutine(WaitAndLoad());
+    }
+
+    private IEnumerator WaitAndLoad()
+    {
+        yield return null;
+        operation = SceneManager.LoadSceneAsync(0, LoadSceneMode.Single);
     }
 }
